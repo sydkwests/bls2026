22a3fada7b14f07c5537a2ab4f7180a08ec0f5c8
Syed Furqaan Ahmed <sydkwests@gmail.com>
2026-01-31 15:11:06 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -98,7 +98,7 @@ const Contact: React.FC = () => {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.3567676941164!2d77.56499831482245!3d13.012959990828796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae17d917955555%3A0x6b19a2283e58c8a1!2sRamaiah%20University%20of%20Applied%20Sciences!5e0!3m2!1sen!2sin!4v1677651234567!5m2!1sen!2sin" 
                     width="100%" 
                     height="100%" 
-                    style={{ border: 0, filter: 'grayscale(1) contrast(1.2)', pointerEvents: mapActive ? 'auto' : 'none' }} 
+                    style={{ border: 0, pointerEvents: mapActive ? 'auto' : 'none' }} 
                     allowFullScreen 
                     loading="lazy" 
                     referrerPolicy="no-referrer-when-downgrade"
